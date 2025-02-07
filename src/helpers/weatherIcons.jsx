5a12ed62e5ecb1a99485817781e5eecbc8fce749
J4BC97 <hammerban123@gmail.com>
2025-02-07 09:25:43 -0500

    Actualizacion: Weather App - Toques Finales

--- src/helpers/weatherIcons.jsx
+++ src/helpers/weatherIcons.jsx
@@ -2,14 +2,16 @@ import { FaSun, FaCloud, FaCloudRain, FaSnowflake, FaCloudSun, FaSmog, FaBolt, F
 
 // Función para obtener el ícono del clima basado en la condición
 export const getWeatherIcon = (condition) => {
-  const lowerCondition = condition.toLowerCase();
-  if (lowerCondition.includes('sun') || lowerCondition.includes('clear')) {
+  const lowerCondition = condition.toLowerCase().trim(); // Asegúrate de que no haya espacios adicionales
+  console.log("Condition received:", lowerCondition); // Depuración
+
+  if (lowerCondition.includes('soleado') || lowerCondition.includes('despejado')) {
     return <FaSun className="text-yellow-500 text-4xl" />;
-  } else if (lowerCondition.includes('partly cloudy') || lowerCondition.includes('partly sunny')) {
+  } else if (lowerCondition.includes('parcialmente nublado') || lowerCondition.includes('parcialmente soleado')) {
     return <FaCloudSun className="text-yellow-500 text-4xl" />;
-  } else if (lowerCondition.includes('cloud')) {
+  } else if (lowerCondition.includes('nublado')) {
     return <FaCloud className="text-gray-500 text-4xl" />;
-  } else if (lowerCondition.includes('rain') || lowerCondition.includes('shower')) {
+  } else if (lowerCondition.includes('rain') || lowerCondition.includes('ligeras precipitaciones de aguanieve')) {
     return <FaCloudRain className="text-blue-500 text-4xl" />;
   } else if (lowerCondition.includes('snow')) {
     return <FaSnowflake className="text-blue-300 text-4xl" />;
